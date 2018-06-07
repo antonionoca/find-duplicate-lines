0b9282223da32e36352273832aaad68f9b46f2c8
Antonio Noca <antonionoca@gmail.com>
2018-06-07 21:54:04 +0100

    Checking if the commands are available before running the script

--- find-duplicate-lines.sh
+++ find-duplicate-lines.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
+
+type sort >/dev/null 2>&1 || { echo >&2 "The sort command is required, but it is not installed. Aborting."; exit 1; }
+type uniq >/dev/null 2>&1 || { echo >&2 "The uniq command is required, but it is not installed. Aborting."; exit 2; }
+
 sort $1 | uniq -d
